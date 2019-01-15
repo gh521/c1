de3a11bdf2d7b54f344aff975d4460ff28b17dee
xx <qq@a.com>
2019-01-15 15:25:20 +0800

    first init

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main(){
+int qian=0;
+printf("jrcqglxt");
+while(1)
+{
+    int a=0;
+    printf("1---cq\n");
+    printf("1---qq\n");
+    printf("3---ye\n");
+    printf("4---tc\n");
+    printf("qinshurugongnengbianhao\n");
+    scanf("%d",&a);
+    if(a==1){
+        int cun;
+        char x;
+        printf("jinrucunqiangongnengqinshurujine：\n");
+        scanf("%d",&cun);
+        qian += cun;
+        printf("cunqianchengong,dianjihuichejie\n");
+        scanf("%c",&x);
+        scanf("%c",&x);
+    }
+    }
+return 0;
+    }
