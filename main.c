ffc712b155010f340c6d6cb184fdb9952c3e7207
xx <qq@a.com>
2019-01-15 15:50:23 +0800

    增加取钱功能

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ while(1)
 {
     int a=0;
     printf("1---cq\n");
-    printf("1---qq\n");
+    printf("2---qq\n");
     printf("3---ye\n");
     printf("4---tc\n");
     printf("qinshurugongnengbianhao\n");
@@ -21,6 +21,16 @@ while(1)
         printf("cunqianchengong,dianjihuichejie\n");
         scanf("%c",&x);
         scanf("%c",&x);
+    }
+    if(a==2){
+int qu;
+char x;
+printf("qinshuruquchudejine/n");
+scanf("%d",&qu);
+qian-=qu;
+printf("quqianchengongdianjihuichejixue/n");
+scanf("%c",&x);
+scanf("%c",&x);
     }
     }
 return 0;
